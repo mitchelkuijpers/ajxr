90609e45cadb94b084bd1f073f73d59ee49897ca
Tomas Theunissen <t.p.theunissen@avisi.nl>
2014-02-15 16:52:53 +0100

    Return Q promise

--- src/ajxr.js
+++ src/ajxr.js
@@ -1,5 +1,7 @@
 (function(){
   var request = function(method, url, payload) {
+    var dfd = Q.defer();
+
     var request = new XMLHttpRequest();
     request.open(method, url, true);
     request.onload = onload;
@@ -7,28 +9,32 @@
   
     function onload() {
       if (request.status === 200) {
-        console.log('DONE');
+        var response = JSON.parse(request.responseText);
+
+        dfd.resolve(response);
       } else {
-        console.log('FAIL: ' + request.status);
+        dfd.reject(new Error("Status code was " + request.status));
       }
-    } 
+    }
+
+    return dfd.promise;
   }
 
   var ajxr = {
     get: function(url) {
-      request('GET', url);
+      return request('GET', url);
     },
   
     post: function(url, payload) {
-      request('POST', url, payload);
+      return request('POST', url, payload);
     },
 
     put: function(url, payload) {
-      request('PUT', url, payload);
+      return request('PUT', url, payload);
     },
 
-    delete: function(url, payload) {
-      request('DELETE', url);
+    delete: function(url) {
+      return request('DELETE', url);
     }
   }
 
